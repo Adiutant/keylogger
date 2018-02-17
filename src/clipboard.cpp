f840c437f8c71e62ed8d576f05b12c8a00a4eec2
John <vim2meta@tutanota.com>
2018-02-16 22:24:20 -0500

    return after closing clipboard

--- src/clipboard.cpp
+++ src/clipboard.cpp
@@ -4,22 +4,22 @@
 // Get the current clipboard data.
 std::wstring clipboard::get_data()
 {
+    std::wstring clipboard_data;
+
     if (OpenClipboard(nullptr))
     {
         if (auto handle = GetClipboardData(CF_UNICODETEXT))
         {
             if (const auto data = GlobalLock(handle))
             {
-                const auto clipboard_data = std::wstring{ static_cast<WCHAR*>(data) };
+                clipboard_data = std::wstring{ static_cast<WCHAR*>(data) };
 
                 GlobalUnlock(handle);
-
-                return clipboard_data;
             }
         }
 
         CloseClipboard();
     }
 
-    return {};
+    return clipboard_data;
 }
